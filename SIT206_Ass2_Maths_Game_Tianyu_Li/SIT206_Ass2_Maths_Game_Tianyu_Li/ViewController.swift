240a8dc1bb8f48159e61589a7e6984e0cecc9506
李天宇 <LTY@ltymacbook.local>
2017-04-28 03:13:29 +1000

    1st check in

---
+++ SIT206_Ass2_Maths_Game_Tianyu_Li/SIT206_Ass2_Maths_Game_Tianyu_Li/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIT206_Ass2_Maths_Game_Tianyu_Li
+//
+//  Created by 李天宇 on 2017/4/28.
+//  Copyright © 2017年 Deakin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
